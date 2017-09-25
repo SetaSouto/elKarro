e3864207cd5dd5ada0176682a63bb1351f1e2f8a
fabsouto <fabsouto@cl.ibm.com>
2017-09-25 15:10:29 -0300

    Add product and order models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from products import models_choices
+
+
+class Order(models.Model):
+    """
+    Order that has a name (the buyer), a creation datetime, a state (delivered or not) and products.
+    """
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    delivered = models.BooleanField(default=False)
+    products = models.ManyToManyField(Product)
+
+
+class Product(models.Model):
+    """
+    Product
+    """
+    number = models.IntegerField(choices=models_choices.NUMBERS)
+    # Optional name and description for the product
+    name = models.CharField(blank=True, null=True, max_length=100)
+    description = models.CharField(blank=True, null=True, max_length=500)
